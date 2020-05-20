095d46b703aed12fbf99cb9cf77fdd57236feb47
Julio Chazari <julio.chazari@gmail.com>
2020-05-20 02:04:12 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
       }
     }
 
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,8 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+    
+  programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
@@ -81,6 +84,13 @@ def adding_matz
         :languages => ["C"]
       }
     }
+    
+  programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+  }
+  
+  programmer_hash
 
 end
 
@@ -103,6 +113,11 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    
+  alans_new_info = "GUI"
+  programmer_hash[:alan_kay][:known_for] = alans_new_info
+  
+  programmer_hash
 
 end
 
@@ -125,5 +140,9 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+    
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  
+  programmer_hash
 
 end
